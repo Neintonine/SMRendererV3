b45a10b6764685a4e6bb104a05b707e26873af11
Michel Fedde <michel.game.de.f@gmail.com>
2021-03-27 18:07:52 +0100

    Sorts objects by Transformation

--- SMCode/SM2D/Scene/ItemCollection.cs
+++ SMCode/SM2D/Scene/ItemCollection.cs
@@ -1,7 +1,9 @@
 ﻿#region usings
 
+using System;
 using SM.Base.Scene;
 using SM.Base.Types;
+using SM.Base.Window;
 using SM2D.Types;
 
 #endregion
@@ -15,6 +17,29 @@ namespace SM2D.Scene
         public ItemCollection()
         {
             Transform.Size = new CVector2(1);
+        }
+
+        public override void Draw(DrawContext context)
+        {
+            this.Sort(Comparitor);
+
+            base.Draw(context);
+        }
+
+        private int Comparitor(IShowItem a, IShowItem b)
+        {
+            if (a is ITransformItem<Transformation> ta)
+            {
+                if (b is ITransformItem<Transformation> tb)
+                {
+                    return (int)((ta.Transform.GetMatrix()[3, 2] - tb.Transform.GetMatrix()[3, 2]) * Transformation.ZIndexPercision);
+                }
+                else return 1;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
